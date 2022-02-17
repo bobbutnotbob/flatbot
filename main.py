532c2a4459e5640e38b0f7a2b160d0f638002d62
Emile Reiser <emile.reiser@outlook.co.nz>
2022-02-17 22:14:06 +1300

    added basic shopping list functionality

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 import os
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{bot.user.name} has connected to Discord!')
 
-    print(f'{client.user} has connected to Discord!')
+@bot.group(name='sl', invoke_without_command=True)
+async def shopping_list(ctx):
+    await ctx.send('parent')
 
-client.run(TOKEN)
+@shopping_list.command(name='help')
+async def shopping_help(ctx):
+    await ctx.send('I don\'t work for whatever reason')
+
+@shopping_list.command(name='add')
+async def add_to_shopping_list(ctx, *, item):
+    with open('shopping_list.txt', 'r+') as shopping_list:
+        lc = len(shopping_list.readlines()) + 1
+        md_syntax = f'- [] {lc}. {item}\n'
+        shopping_list.write(md_syntax)
+
+    await ctx.send(f'+ Added `{item}` to the shopping list')
+
+@shopping_list.command(name='remove')
+async def add_to_shopping_list(ctx, line_to_rm: int):
+    index = 1
+    with open('shopping_list.txt', 'r') as shopping_list:
+        lines = shopping_list.readlines()
+    with open('shopping_list.txt', 'w') as new_shopping_list:
+        for line in lines:
+            if index == line_to_rm:
+                item = line[8:]
+                continue
+            else:
+                shopping_list.write(line)
+    
+    await ctx.send(f'— Removed `{item}` from the shopping list')
+            
+@shopping_list.command(name='clear')
+async def clear_shopping_list(ctx):
+    with open('shopping_list.txt', 'w') as shopping_list:
+        shopping_list.write('')
+
+    await ctx.send('Cleared the shopping list')
+
+@shopping_list.command(name='print')
+async def add_to_shopping_list(ctx):
+    await ctx.send('I am the shopping list')
+
+bot.run(TOKEN)
