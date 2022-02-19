4247ef42c942fbfaacfd2a4ce26b9a2bd6706036
Emile Reiser <emile.reiser@outlook.co.nz>
2022-02-19 16:06:55 +1300

    fixed !sl remove function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
@@ -24,24 +23,32 @@ async def shopping_help(ctx):
 async def add_to_shopping_list(ctx, *, item):
     with open('shopping_list.txt', 'r+') as shopping_list:
         lc = len(shopping_list.readlines()) + 1
+        regular_syntax = f'{lc}. {item}\n'
         md_syntax = f'- [] {lc}. {item}\n'
-        shopping_list.write(md_syntax)
+        shopping_list.write(regular_syntax)
 
     await ctx.send(f'+ Added `{item}` to the shopping list')
-
+    
 @shopping_list.command(name='remove')
 async def add_to_shopping_list(ctx, line_to_rm: int):
-    index = 1
     with open('shopping_list.txt', 'r') as shopping_list:
         lines = shopping_list.readlines()
+
+    index = 1
+    new_line_num = 0
     with open('shopping_list.txt', 'w') as new_shopping_list:
         for line in lines:
+            print(line, index)
             if index == line_to_rm:
-                item = line[8:]
-                continue
+                item = line[3:]
+
             else:
-                shopping_list.write(line)
-    
+                new_line_num += 1
+                line = line[3:]
+                new_shopping_list.write(f'{new_line_num}. {line}')
+
+            index += 1
+                
     await ctx.send(f'— Removed `{item}` from the shopping list')
             
 @shopping_list.command(name='clear')
