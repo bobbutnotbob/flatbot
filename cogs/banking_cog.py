5e016df459e49dd99434aabb049346a46ee74a1b
Emile Reiser <emile.reiser@outlook.co.nz>
2023-04-24 21:23:54 +1200

    began replacing buttons with reactions

--- cogs/banking_cog.py
+++ cogs/banking_cog.py
@@ -12,10 +12,11 @@ DATABASEDIR = os.getenv('DATABASE_LOCATION')
 GUILD = os.getenv('DISCORD_GUILD')
 
 # Payment buttons
+'''
 class PaymentView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-
+self
     @discord.ui.button(label="Pay", style=discord.ButtonStyle.green, custom_id="pay_button")
     async def pay(self, button: discord.ui.Button, interaction: discord.Interaction):
         user_id = interaction.user.id
@@ -66,6 +67,7 @@ class PaymentView(discord.ui.View):
                     response = 'You do not have permission to delete this payment'
         
         await interaction.response.send_message(response, ephemeral=True)
+'''
 
     
 class BankingCog(commands.Cog):
@@ -74,24 +76,35 @@ class BankingCog(commands.Cog):
     
     bank_transfer = SlashCommandGroup('bank-transfer', 'Commands related to bank transfers')
     
+    # Command for creating a payment request
     @bank_transfer.command(description='Request payment from flat members')
     async def request(self, ctx, target: discord.abc.Mentionable, amount: float, description = "No description provided"):
+
+        # Round inputted request amount to 2 decimal places, get the string required to mention the targeted user/role
         amount = round(amount, 2)
         mention_str = target.mention
 
+        # If target is '@everyone', set the mention string to '@everyone' (mentioning the role id of @everyone freaks it out)
         if target.id == ctx.guild_id:
             mention_str = '@everyone'
 
+        # Determines whether target is a role or a single user and populates the to_pay list accordingly
         if hasattr(target, 'members'):
             to_pay = [member.id for member in target.members]
         else:
             to_pay = [target.id]
 
+        # Connect to the database and retrieve list of valid targets for requesting payment
         async with aiosqlite.connect(f'{DATABASEDIR}/{ctx.guild.id}.db') as db:
             async with db.execute('SELECT UserId FROM Users') as cursor:
                 userlist = await cursor.fetchall()
                 userlist = [user for (user,) in userlist]
+
+                # Check whether each user in the to_pay list is a valid target
+                    # If target is not valid and there are multiple targets in to_pay, remove the target from list
+                    # If to_pay contains only 1 user, fail to create the payment request
                 for user in to_pay:
+                    # TODO: Currently provides no feedback on whether a target is valid or not. If multiple targets are removed and the payment fails, only the last target will be mentioned in the response
                     if user not in userlist:
                         if len(userlist) == 1:
                             print('Gah!')
@@ -102,8 +115,12 @@ class BankingCog(commands.Cog):
                             to_pay.remove(user)
                             print(f'Failed adding user with ID {user} as they are not in the database')
 
-            await ctx.respond(f'**To:** {ctx.author.mention}\n**From:** {mention_str}\n**Amount:** ${amount}\n**Description:** {description}', view=PaymentView())
+            # Respond with a text message containing the details of the payment request, and add a checkmark reaction to the message
+            await ctx.respond(f'**To:** {ctx.author.mention}\n**From:** {mention_str}\n**Amount:** ${amount}\n**Description:** {description}')
             message = await ctx.interaction.original_message()
+            await message.add_reaction('✅')
+            
+            # Commit the details of the request into the database
             await db.execute('PRAGMA foreign_keys = ON')
             query = f'INSERT INTO Payments VALUES ({message.id}, {amount}, "{description}", {ctx.author.id}, "{to_pay}", "{to_pay}")'
             await db.execute(query)
--- main.py
+++ main.py
@@ -18,6 +18,65 @@ bot = discord.Bot(debug_guilds=[GUILD], allowed_mentions=discord.AllowedMentions
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     print('----------')
+
+@bot.event
+async def on_raw_reaction_add(reaction):
+    # If the member reacting to the message is a bot, or using an emoji we don't care about, do nothing
+    if reaction.member.bot:
+        return
+    if str(reaction.emoji) != '✅':
+        return
+    
+    # Set some variables from the context of the reaction
+    message_id = reaction.message_id
+    user_id = reaction.user_id
+    reaction_channel = bot.get_channel(reaction.channel_id)   
+    message = await reaction_channel.fetch_message(message_id)
+
+    # If the message being reacted to does not already contain a reaction from the bot, return
+    bot_reacted = False
+    for existing_reaction in message.reactions:
+        async for existing_reactor in existing_reaction.users():
+            if existing_reactor.id == bot.user.id:
+                bot_reacted = True
+                break
+    if not bot_reacted:
+        return
+
+    # Connect to db and attempt to find a payment entry for the message that is being reacted to
+    async with aiosqlite.connect(f'{DATABASEDIR}/{reaction.guild_id}.db') as db:
+        async with db.execute(f'SELECT RequestedFrom, UnpaidBy FROM Payments WHERE MessageId = {message_id}') as cursor:
+            db_data = await cursor.fetchone()
+            # If no matching payment is found, return
+            if db_data == None:
+                await reaction_channel.send('Unable to find payment in database')
+                return
+            
+            # Otherwise, deserialize the 'RequestedFrom' and 'UnpaidBy' JSON from the db and store them in lists
+            requested_from = json.loads(db_data[0])
+            unpaid_by = json.loads(db_data[1])
+            
+            if user_id not in requested_from:
+                response = 'You are not required to pay this'
+            
+            elif user_id not in unpaid_by:
+                response = 'You have already payed this'
+
+            # If all checks pass, remove the user from the 'unpaid_by' list, reserialize it and commit it back into the db
+            else:
+                unpaid_by.remove(user_id)
+                unpaid_by = json.dumps(unpaid_by)
+                await cursor.execute(f'UPDATE Payments SET UnpaidBy = "{unpaid_by}" WHERE MessageId = {message_id}')
+                await db.commit()
+                response = 'Successfully updated payment'
+                
+        await reaction_channel.send(response)
+
+@bot.event
+async def on_raw_reaction_remove(reaction):
+    reaction_channel = bot.get_channel(reaction.channel_id)
+    await reaction_channel.send('Bazinga!')
+    return
   
 # Load extensions/cogs
 bot.load_extension('cogs.banking_cog')
